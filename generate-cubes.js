7f808e3c165aa270f64620332782ca61073a4b23
Adam Chambers <adammchambers@gmail.com>
2022-01-08 19:00:49 -0500

    Move shared functions to helper fine

--- generate-cubes.js
+++ generate-cubes.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const path = require('path')
-const puppeteer = require('puppeteer')
+const helpers = require('./lib/helpers')
 
 const totalCubes = 999
 const hues = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink']
@@ -37,85 +37,6 @@ const cubeNums = [
 ]
 const json = { cubes: [] }
 
-const random = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-const copyFileSync = (source, target) => {
-  let targetFile = target
-
-  // If target is a directory, a new file with the same name will be created
-  if (fs.existsSync(target)) {
-    if (fs.lstatSync(target).isDirectory()) {
-      targetFile = path.join(target, path.basename(source))
-    }
-  }
-
-  fs.writeFileSync(targetFile, fs.readFileSync(source))
-}
-
-const copyFolderRecursiveSync = (source, target) => {
-  let files = []
-
-  // Check if folder needs to be created or integrated
-  const targetFolder = path.join(target, path.basename(source))
-  if (!fs.existsSync(targetFolder)) {
-    fs.mkdirSync(targetFolder)
-  }
-
-  // Copy
-  if (fs.lstatSync(source).isDirectory()) {
-    files = fs.readdirSync(source)
-    files.forEach(function (file) {
-      var curSource = path.join(source, file)
-      if (fs.lstatSync(curSource).isDirectory()) {
-        copyFolderRecursiveSync(curSource, targetFolder)
-      } else {
-        copyFileSync(curSource, targetFolder)
-      }
-    })
-  }
-}
-
-const captureScreens = async (cubes) => {
-  let browser = null
-
-  try {
-    // launch headless Chromium browser
-    browser = await puppeteer.launch({
-      headless: true,
-    })
-    // create new page object
-    const page = await browser.newPage()
-
-    // set viewport width and height
-    await page.setViewport({
-      width: 1080,
-      height: 1080,
-    })
-
-    for (const { id } of cubes) {
-      // console.log(
-      //   'file://' + path.join(__dirname, `./dist/cubes/${id}/index.html`)
-      // )
-      await page.goto(
-        'file://' + path.join(__dirname, `./dist/cubes/${id}/index.html`)
-      )
-      await page.waitForTimeout(3000)
-      await page.screenshot({ path: `./dist/cubes/${id}/image.png` })
-      console.log(`Screenshot captured: ${id}`)
-      await page.waitForTimeout(500)
-    }
-  } catch (err) {
-    console.log(`Screenshot Error: ${err.message}`)
-  } finally {
-    if (browser) {
-      await browser.close()
-    }
-    console.log(`${cubes.length} screenshots captured.`)
-  }
-}
-
 // build cube number map
 for (let i = 0; i < cubeNums.length; i++) {
   for (let j = 0; j < cubeNums[i].numFrequency; j++) {
@@ -127,8 +48,8 @@ for (let i = 0; i < cubeNums.length; i++) {
 console.log('**Generating space cubes**')
 console.log('--------------------------\r\n')
 for (let i = 0; i < totalCubes; i++) {
-  const hue = hues[random(0, hues.length - 1)]
-  const cubesNum = cubes[random(0, cubes.length - 1)]
+  const hue = hues[helpers.random(0, hues.length - 1)]
+  const cubesNum = cubes[helpers.random(0, cubes.length - 1)]
 
   console.log(`New Space Cube: ${i}`)
   console.log(`Hue: ${hue}\r\nCubes: ${cubesNum}\r\n `)
@@ -142,19 +63,19 @@ for (let i = 0; i < totalCubes; i++) {
   fs.mkdir('./dist/', () => {
     fs.mkdir('./dist/cubes/', () => {
       fs.mkdir(`./dist/cubes/${i}/`, () => {
-        copyFolderRecursiveSync(
+        helpers.copyFolderRecursiveSync(
           './template/js',
           path.join(__dirname, `./dist/cubes/${i}/`)
         )
-        copyFolderRecursiveSync(
+        helpers.copyFolderRecursiveSync(
           './template/lib',
           path.join(__dirname, `./dist/cubes/${i}/`)
         )
-        copyFolderRecursiveSync(
+        helpers.copyFolderRecursiveSync(
           './template/css',
           path.join(__dirname, `./dist/cubes/${i}/`)
         )
-        copyFileSync(
+        helpers.copyFileSync(
           './template/index.html',
           path.join(__dirname, `./dist/cubes/${i}/`)
         )
@@ -216,6 +137,4 @@ fs.writeFile(
 console.log(`\r\n**Capturing Screenshots**`)
 console.log('-------------------------\r\n')
 
-// const newJson = require('./dist/meta.json')
-// captureScreens(newJson.cubes.slice(330))
-captureScreens(json.cubes)
+helpers.captureScreens(json.cubes)
--- organise-cubes.js
+++ organise-cubes.js
@@ -1,24 +1,12 @@
 const fs = require('fs')
 const path = require('path')
 const cubesJSON = require('./dist/meta.json')
-
-const copyFileSync = (source, target) => {
-  let targetFile = target
-
-  // If target is a directory, a new file with the same name will be created
-  if (fs.existsSync(target)) {
-    if (fs.lstatSync(target).isDirectory()) {
-      targetFile = path.join(target, path.basename(source))
-    }
-  }
-
-  fs.writeFileSync(targetFile, fs.readFileSync(source))
-}
+const helpers = require('./lib/helpers')
 
 cubesJSON.cubes.forEach((cube) => {
   fs.mkdir('./dist/cube-images', () => {
     fs.mkdir(`./dist/cube-images/${cube.cubes}`, () => {
-      copyFileSync(
+      helpers.copyFileSync(
         `./dist/cubes/${cube.id}/image.png`,
         path.join(
           __dirname,
