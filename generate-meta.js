85df0637a0f6d5b4bafec23ba8c7cad139defbf1
Adam Chambers <adammchambers@gmail.com>
2022-01-12 23:40:28 -0500

    Uninstall unique name generator and update meta data

--- generate-meta.js
+++ generate-meta.js
@@ -1,29 +1,21 @@
 const fs = require('fs')
 const path = require('path')
-const uniqueNamesGenerator = require('unique-names-generator')
 const meta = require('./dist/meta.json')
 
-const ipfsUrl = 'ipfs://QmP8AEkpHHMdjfMUH84efNjKtSt75P3LBXkoezwRYgtaxG'
+const ipfsUrl = 'ipfs://QmYCpnoBpgYwzCnLKFBuh4bBYdhiB85DiNR45NvcPQC43V'
 
 meta.cubes.map((cube) => {
   fs.mkdir('./dist/meta/', () => {
     fs.writeFile(
       path.join(__dirname, `./dist/meta/${cube.id}`),
       JSON.stringify({
-        description: 'Space Cube',
+        description: 'Space Cubes is a collection of 999 interactive NFTs created by chambaz.eth (@chambaz)',
         image: `${ipfsUrl}/${cube.id}/image.png`,
         animation_url: `${ipfsUrl}/${cube.id}/index.html`,
-        name: uniqueNamesGenerator.uniqueNamesGenerator({
-          dictionaries: [
-            uniqueNamesGenerator.adjectives,
-            [cube.color],
-            uniqueNamesGenerator.animals,
-          ],
-          separator: '-',
-        }),
+        name: `Space Cube #${cube.id}`,
         attributes: [
           { trait_type: 'Color', value: cube.color },
-          { trait_type: 'Cubes', value: Math.pow(cube.cubes, 3) },
+          { trait_type: 'Cubes', value: Math.pow(cube.cubes, 3).toString() },
         ],
       }),
       (err) => {
