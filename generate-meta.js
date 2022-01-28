b28b0cebf7ceff04b7f9d212b46226c79f56c31b
Adam Chambers <adammchambers@gmail.com>
2022-01-28 18:16:10 -0500

    Update IPFS url to Pinata domain

--- generate-meta.js
+++ generate-meta.js
@@ -2,16 +2,15 @@ const fs = require('fs')
 const path = require('path')
 const meta = require('./dist/meta.json')
 
-const ipfsUrl = 'ipfs://QmYCpnoBpgYwzCnLKFBuh4bBYdhiB85DiNR45NvcPQC43V'
-
 meta.cubes.map((cube) => {
   fs.mkdir('./dist/meta/', () => {
     fs.writeFile(
       path.join(__dirname, `./dist/meta/${cube.id}`),
       JSON.stringify({
-        description: 'Space Cubes is a collection of 999 interactive NFTs created by chambaz.eth (@chambaz)',
-        image: `${ipfsUrl}/${cube.id}/image.png`,
-        animation_url: `${ipfsUrl}/${cube.id}/index.html`,
+        description:
+          'Space Cubes is a collection of 999 interactive NFTs created by chambaz.eth (@chambaz)',
+        image: `ipfs://QmcGJWJYFt8iwYSCHdRcoXPHj7f6RYmuNqdHhiiiTwhzfj/${cube.id}/image.png`,
+        animation_url: `https://spacecubes.mypinata.cloud/ipfs/QmcGJWJYFt8iwYSCHdRcoXPHj7f6RYmuNqdHhiiiTwhzfj/${cube.id}/index.html`,
         name: `Space Cube #${cube.id}`,
         attributes: [
           { trait_type: 'Color', value: cube.color },
